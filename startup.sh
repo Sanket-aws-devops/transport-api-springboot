dc717db38d428f187625977524d23699c1ff38eb
Sanket-aws-devops <157350422+Sanket-aws-devops@users.noreply.github.com>
2024-12-23 17:12:53 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
-    sudo apt-get update -y
-    cd /home/ubuntu/transport-api-springboot
-    sudo apt-get install openjdk-11-jdk -y
-    sudo apt install snapd -y
-    sudo snap install gradle --classic
-    gradle build
-    cd /build/libs/transport-module-1.0.jar
-    nohup java -jar /build/libs/transport-module-1.0.jar > /tmp/app.log 2>&1 &
+    #sudo apt-get update -y
+    #c#d /home/ubuntu/transport-api-springboot
+    #sudo apt-get install openjdk-11-jdk -y
+    #sudo apt install snapd -y
+    #sudo snap install gradle --classic
+    #gradle build
+    #cd /build/libs/transport-module-1.0.jar
+    #nohup java -jar /build/libs/transport-module-1.0.jar > /tmp/app.log 2>&1 &
+cd /home/ubuntu
+nohup java -jar transport-module-1.0.jar > app.log 2>&1 &
